52c1262fb04695ad9c3855909d644ea11e2b718f
nicolasavilab <nicolasab1996@gmail.com>
2024-09-14 17:57:46 -0300

    created Project

---
+++ composeApp/src/commonMain/kotlin/org/pokeapipokedex/project/App.kt
@@ -0,0 +1,17 @@
+package org.pokeapipokedex.project
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Column() {
+            Text("Compose: wn wnw wnwiiwfweife")
+        }
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/pokeapipokedex/project/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.pokeapipokedex.project
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
